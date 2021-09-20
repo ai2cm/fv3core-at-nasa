000b3fb7bcfc9120aba63050ff37384b8eb3ce1b
Florian <floriand@vulcan.com>
2021-09-20 07:56:07 -0700

    Make sure singularity is using local scratch disk for pulling the image on PRISM

--- prism_singularity_pull_sandbox.sh
+++ prism_singularity_pull_sandbox.sh
@@ -10,4 +10,7 @@ if [[ ! -e nobackup_tmp ]]; then
 fi
 
 cd $NOBACKUP
+mkdir -p /lscratch/$USER/singularity_cache
+export SINGULARITY_TMPDIR=/lscratch/$USER/singularity_cache
+export SINGULARITY_CACHEDIR=/lscratch/$USER/singularity_cache
 singularity build --sandbox ./prism_fv3core_sandbox docker://gitlab.nccs.nasa.gov:5050/fgdeconi/fv3core-at-nasa/fv3core_ubuntu18
